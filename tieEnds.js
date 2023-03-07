ebea84d0614df1220a9360102dc7ab0efb46fabb
Tu1234-lang <70562263+Tu1234-lang@users.noreply.github.com>
2023-03-06 21:19:56 -0500

    Delete tieEnds.js

--- tieEnds.js
+++
@@ -1,6 +0,0 @@
-async function fillTheNum(){
-	url = "https://cloudresumepy.azurewebsites.net/api/httptrigger310";
-	post = await fetch(url).then((res) => res.json());
-	document.getElementById("num").innerText = post.PartitionKey;
-}
-fillTheNum();
